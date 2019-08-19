126d32315c00eaf9657581392e68480a9ce42c04
Erwin Thompson Fung <erwin@qratedcrates.com>
2019-08-19 15:48:13 +0800

    added build

---
+++ build/precache-manifest.217023f859459f608011521545a5849f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641d11fe306971609339ae8b8ce96722",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745e4389627b9496bd4f",
+    "url": "/static/css/2.5fb67fc3.chunk.css"
+  },
+  {
+    "revision": "4dad245505802858d96f",
+    "url": "/static/css/main.ee992bb6.chunk.css"
+  },
+  {
+    "revision": "745e4389627b9496bd4f",
+    "url": "/static/js/2.8f73e151.chunk.js"
+  },
+  {
+    "revision": "4dad245505802858d96f",
+    "url": "/static/js/main.02ee2368.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
